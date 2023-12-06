f8cc8c8527ab58aa5f9c95a3c4c891ac23867a9e
Michael Kovach <26michaelkovach@usd266.com>
2023-12-06 10:00:54 -0600

    Done

--- 07StringsAsLists/Assignment/main.py
+++ 07StringsAsLists/Assignment/main.py
@@ -82,5 +82,72 @@ print("---alternate_case---")
 print(alternate_case("hello"))
 print(alternate_case("better"))
 
-def remove_vowels():
+def remove_vowels(word):
+    result = ""
+    for letter in word:
+        if letter in "aeiou":
+            pass
+        else:
+            result = result + letter
+    return result
+print("---remove_vowels---")
+print(remove_vowels("bazinga"))
+print(remove_vowels("jello"))
+
+def to_camel_case(string):
+    result = ""
+    nextcapital = False
+    for character in string:
+        if character == " ":
+            nextcapital = True
+        else:
+            if nextcapital == True:
+                result = result + character.upper()
+                nextcapital = False
+            elif nextcapital == False:
+                result = result + character.lower()
+    return result
+print("---to_camel_case---")
+print(to_camel_case("hi im josh"))
+print(to_camel_case("how is your day"))
+
+def to_snake_case(string):
+    result = ""
+    underscore = "_"
+    for character in string:
+        if character == " ":
+            result = result + underscore
+        else:
+            result = result + character
+    return result
+print("---to_snake_case---")
+print(to_snake_case("i want a small drink please"))
+print(to_snake_case("go fish"))
+
+def without_duplicates(list):
+    banlist = "a"
+    result = []
+    for integer in list:
+        if integer == banlist:
+            banlist = integer
+            pass
+        else:
+            result.append(integer)
+            banlist = integer
+    return result
+print("---without_duplicates---")
+print(without_duplicates([1, 2, 3, 4, 2, 5]))
+print(without_duplicates([1, 1, 1, 1, 3, 1, 1, 3]))
+
+def filter_valid_act_scores(list):
+    result = []
+    for score in list:
+        if score >= 1 and score <= 36:
+            result.append(score)
+        else:
+            pass
+    return result
+print("---filter_valid_act_scores---")
+print(filter_valid_act_scores([1, 2, 50, 46, 19]))
+print(filter_valid_act_scores([27, 32, 36, 48, 0, 29]))
 
